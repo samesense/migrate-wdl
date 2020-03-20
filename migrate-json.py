3f48213920634a7f57cc0cd32b5d6f36c70dd3e0
samesense <samesense@gmail.com>
2020-03-20 09:52:21 -0400

    Update json, but no gs cp.

---
+++ migrate-json.py
@@ -0,0 +1,78 @@
+"""Replace public gs image url w/ project gs.
+   Cp data to my project.
+   (data cannot be copied directly between buckets)
+"""
+import argparse
+import os
+import json
+
+parser = argparse.ArgumentParser(description="Migrate broad json files to gcp vpc")
+parser.add_argument("-d", "--delete", action="store_true", help="Delete local gs data.")
+parser.add_argument("project", type=str, help="GCP project")
+parser.add_argument("json", type=str, help="Input json.")
+parser.add_argument("json_out", type=str, help="Output json.")
+
+
+def strip_gs(gs_url):
+    return gs_url[5:]
+
+
+def mk_new_gs(project, gs_url):
+    """
+    gs://gatk-test-data/intervals/hg38_wgs_scattered_calling_intervals.txt
+    """
+    path = strip_gs(gs_url)
+    return f"gs://{project}/data/ref-data/{path}"
+
+
+def update_json(project, jsn, json_out):
+    """Return set of gcs urls."""
+    urls = set()
+    with open(jsn) as f:
+        j = json.load(f)
+        for field in j:
+            val = j[field]
+            if isinstance(val, str):
+                if val[:5].startswith("gs://"):
+                    gs_url = val
+                    new_gs = mk_new_gs(project, gs_url)
+                    j[field] = new_gs
+                    urls.add((gs_url, new_gs))
+
+    with open(json_out, "w") as fout:
+        json.dump(j, fout)
+
+    return urls
+
+
+def cp_data(delete, project, url):
+    """
+    """
+    src, dest = url
+    src_set = set()
+    local = strip_gs(url)
+    src_set.add(local)
+    os.system(f"gsutil cp {src} .")
+    os.system(f"gsutil cp {local} {dest}")
+
+    # recursive cp data
+    # if dest.endswith('.txt'):
+
+    if delete:
+        for local in src_set:
+            os.system(f"rm {local}")
+
+
+def migrate(delete_data, project, jsn, json_out):
+    urls = update_json(project, jsn, json_out)
+    # for url in urls:
+    #     cp_data(delete_data, project, url)
+
+
+def main(args=None):
+    args = parser.parse_args(args=args)
+    migrate(args.delete, args.project, args.json, args.json_out)
+
+
+if __name__ == "__main__":
+    main()
